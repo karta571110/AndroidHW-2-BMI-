0f90d338c08cadc1f0e954f3c1d35eb788f4ccc7
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-10-14 11:08:17 +0800

    test

--- app/src/main/java/com/example/androidhw_2/MainActivity.java
+++ app/src/main/java/com/example/androidhw_2/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.androidhw_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText h;
+    EditText w;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        h=(EditText)findViewById(R.id.high);
+        w=(EditText)findViewById(R.id.weight);
     }
 }
