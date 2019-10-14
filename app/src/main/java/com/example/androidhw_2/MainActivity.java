b2a0968709681102a8b1d2729f38a197ebecab19
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-10-14 12:00:22 +0800

    456

--- app/src/main/java/com/example/androidhw_2/MainActivity.java
+++ app/src/main/java/com/example/androidhw_2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidhw_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
         h=(EditText)findViewById(R.id.high);
         w=(EditText)findViewById(R.id.weight);
+        Button submit=(Button)findViewById(R.id.submit);
+
+        submit.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View arg0){
+                if(!("".equals(h.getText().toString()) ||
+                        "".equals(w.getText().toString()))){
+                    float fh =Float.parseFloat(h.getEditableText().toString());
+                    float fw=Float.parseFloat(w.getEditableText().toString());
+                    float fresult;
+                }
+            }
+
+        });
     }
+
 }
