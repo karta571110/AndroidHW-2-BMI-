d92531fa4121436f20cc24ed0208483edb889116
karta571110 <karta571110@gmail.com>
2019-10-15 23:06:11 +0800

    完成

--- app/src/main/java/com/example/androidhw_2/MainActivity.java
+++ app/src/main/java/com/example/androidhw_2/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
     EditText h;
     EditText w;
@@ -24,14 +26,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View arg0){
                 if(!("".equals(h.getText().toString()) ||
                         "".equals(w.getText().toString()))){
-                    double fh =Float.parseFloat(h.getEditableText().toString());
+                    float fh =Float.parseFloat(h.getEditableText().toString());
                     float fw=Float.parseFloat(w.getEditableText().toString());
                     float fresult;
                     TextView result=(TextView)findViewById((R.id.result));
                     fh=fh/100;
-                    fh=Math.pow(fh,2);
+                    fh=fh*fh;
 
+                    NumberFormat nf=NumberFormat.getInstance();
+                    nf.setMaximumFractionDigits(2);
+                    fresult=fw/fh;
+                    result.setText(nf.format(fw/fh)+"");
+                    TextView dia =(TextView)findViewById(R.id.textView2);
 
+                    if (fresult<18.5)
+                        dia.setText("體重過輕");
+                    else if (18.5 <= fresult && fresult< 24)
+                        dia.setText("正常範圍");
+                    else if (24 <=fresult && fresult < 27)
+                        dia.setText("過    重");
+                    else if (27 <=fresult && fresult < 30)
+                        dia.setText("輕度肥胖");
+                    else if (30 <= fresult && fresult < 35)
+                        dia.setText("中度肥胖");
+                    else if (fresult >= 35)
+                        dia.setText("重度肥胖");
                 }
             }
 
